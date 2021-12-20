3feb0334360872b8c2644faf6cf219f4f3bd79c2
Gouson <gouson@qq.com>
2021-12-20 18:23:38 +0800

    0

--- JS/get.js
+++ JS/get.js
@@ -5,5 +5,10 @@ var obj = {
 }
 
 const round = obj.__lookupGetter__('a') //该方法已废弃,但是兼容性很好
+
 const round2 = Object.getOwnPropertyDescriptor(obj, 'a').get
-console.log(round, round2)
+
+const round3 = obj.__defineGetter__('a', function() {
+    return 'get a'
+})
+console.log(round, round2, obj.a)
